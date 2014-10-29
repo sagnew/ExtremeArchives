2da06bf344bba9280ca263a2d90a539075107cb7
Sam Agnew <sagnew@eden.rutgers.edu>
2014-10-29 15:59:35 -0400

    Configure settings to use postgres

--- ExtremeArchives/ExtremeArchives/settings.py
+++ ExtremeArchives/ExtremeArchives/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'ExtremeArchives.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'archives',
+        'USER': 'admin',
+        'PASSWORD': 'fucktobias',
+        'HOST': 'localhost',
+        'PORT': '8500'
     }
 }
 
